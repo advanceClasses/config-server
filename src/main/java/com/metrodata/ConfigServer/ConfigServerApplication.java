e2bf7b43a14e0cee01b4c33e992953afc59b5826
johan <johanwirasugianto@gmail.com>
2023-08-31 15:01:52 +0700

    Configuration server

---
+++ src/main/java/com/metrodata/ConfigServer/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.metrodata.ConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+		System.out.println("===== Config Server is Running =====");
+	}
+
+}
